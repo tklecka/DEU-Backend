b273e7941059f54360d26ed3722a82f53968aad5
Tim-Matthias Klecka <tim@klecka.at>
2023-12-30 18:39:32 +0100

    init backend project

---
+++ DEU-Backend/DeuDbContext.cs
@@ -0,0 +1,33 @@
+﻿using DEU_Backend.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace DEU_Backend
+{
+    public class DeuDbContext : DbContext
+    {
+        private readonly DatabaseConfigurationService _dbConfigService;
+
+        public DeuDbContext(DbContextOptions<DeuDbContext> options, DatabaseConfigurationService dbConfigService)
+            : base(options)
+        {
+            _dbConfigService = dbConfigService;
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var dbType = _dbConfigService.GetDatabaseType();
+            var connectionString = _dbConfigService.GetConnectionString();
+
+            if (dbType == "PostgreSQL")
+            {
+                optionsBuilder.UseNpgsql(connectionString);
+            }
+            else if (dbType == "SQLite")
+            {
+                optionsBuilder.UseSqlite(connectionString);
+            }
+        }
+
+    }
+}
---
+++ DEU-Backend/Services/DatabaseConfigurationService.cs
@@ -0,0 +1,25 @@
+﻿namespace DEU_Backend.Services
+{
+    public class DatabaseConfigurationService
+    {
+        private readonly IConfiguration _configuration;
+
+        public DatabaseConfigurationService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetDatabaseType() => _configuration.GetValue<string>("DatabaseConfig:DatabaseType") ?? "SQLite";
+
+        public string GetConnectionString()
+        {
+            var dbType = GetDatabaseType();
+            return dbType switch
+            {
+                "PostgreSQL" => _configuration.GetValue<string>("DatabaseConfig:ConnectionStrings:PostgreSQL") ?? throw new Exception("PostgreSQL connection string is not defined"),
+                "SQLite" => _configuration.GetValue<string>("DatabaseConfig:ConnectionStrings:SQLite") ?? throw new Exception("SQLite connection string is not defined"),
+                _ => throw new Exception("Database type is not defined")
+            };
+        }
+    }
+}
